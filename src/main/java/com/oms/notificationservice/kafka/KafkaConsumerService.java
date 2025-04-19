b14d5d20c129590b430e770c2c60c21d53760e01
abrar12 <mabrar91@gmail.com>
2025-04-19 19:43:06 +0530

    feat: added OrderEvent DTO and integrated Kafka producer with event payload

--- src/main/java/com/oms/notificationservice/kafka/KafkaConsumerService.java
+++ src/main/java/com/oms/notificationservice/kafka/KafkaConsumerService.java
@@ -1,14 +1,30 @@
 package com.oms.notificationservice.kafka;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oms.notificationservice.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerService {
 
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumerService.class);
+    private final ObjectMapper objectMapper;
+
+    public KafkaConsumerService(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @KafkaListener(topics = "order-events", groupId = "notification-group")
     public void consume(String message) {
-        System.out.println("🔔 Notification Received: " + message);
-        // simulate sending email/SMS here
+        try {
+            OrderEvent event = objectMapper.readValue(message, OrderEvent.class);
+            log.info("🔔 Notification received for Order: {}, Status: {}, Message: {}",
+                    event.getOrderId(), event.getStatus(), event.getMessage());
+        } catch (Exception e) {
+            log.error("❌ Failed to deserialize message: {}", message, e);
+        }
     }
 }
